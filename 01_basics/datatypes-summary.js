febf8646253c3c5fc76f67d85c6c10be743890c3
Sohandas097 <128880894+Sohandas097@users.noreply.github.com>
2024-02-03 19:26:31 +0000

    summary with stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,17 +2,17 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n // we write n at last to represent bigint
 
@@ -22,17 +22,17 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
@@ -51,3 +51,40 @@ Arrays => object
 Function => function
 Object => object
 */
+
+// ***************************************Stack & Heap memory**********************************************************
+
+/* 
+
+stack memory used in primitive
+-> you get copy of variable
+
+
+heap memory used in non-primitive
+-> you get reference of any variable
+
+*/
+// stack
+
+let myYoutubeName="Sohan"
+
+
+let anotherName=myYoutubeName
+anotherName="chai aur code"
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+//Heap
+
+let user1 = {
+    email: "abc@gmail.com",
+    upi: "user@paytm"
+}
+
+let user2=user1
+
+user2.email="Sohan@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
