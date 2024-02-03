eaf384a6d133719e60933074c8328cc9ac86176f
Sohandas097 <128880894+Sohandas097@users.noreply.github.com>
2024-02-03 16:28:55 +0000

    conversion on datatypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,28 +1,65 @@
-let score= undefined
+// let score= undefined
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber) 
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) 
+
 /* when valueInNumber=33abc or undefined or any string like "sohan" then output will be NaN -> Not a Number 
    Nan is a special type
    when valueInNumber=null then output will be 0
    when valueInNumber=true then output will be 1
 */
 
-let isLoggeIn="Sohan"
-let booleanIsLoggedIn = Boolean(isLoggeIn)
-console.log(booleanIsLoggedIn) 
+// let isLoggeIn="Sohan"
+// let booleanIsLoggedIn = Boolean(isLoggeIn)
+// console.log(booleanIsLoggedIn) 
+
 /* 
 here isLoggedIn was 1 then the output was true
 now input was ""(means empty string) then output was false
 now input was "Sohan" then output was true
 */ 
 
-let someNumber=33
+// let someNumber=33
 
-let stringNumber=String(someNumber)
-console.log(stringNumber) // -> 33
-console.log(typeof(stringNumber)) // -> string
+// let stringNumber=String(someNumber)
+// console.log(stringNumber) // -> 33
+// console.log(typeof(stringNumber)) // -> string
+
+// **************************************Operations****************************************************
+
+let value=3
+let negValue= -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // -> to the power
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1="hello"
+// let str2=" sohan"
+
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1" + 2); // -> 12
+// console.log(1 + "2"); // -> 12
+// console.log("1" + 2 + 2); // -> 122
+// console.log(1 + 2 + "2"); // -> 32
+
+// console.log(+true); // -> 1 // console.log(true+); -> error
+// console.log(+""); // -> 0
+
+let num1, num2, num3
+
+num1=num2=num3= 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
