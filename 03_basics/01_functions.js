e26c095fe50f200c21979bb32ddb36103bc54eec
Sohandas097 <128880894+Sohandas097@users.noreply.github.com>
2024-02-09 18:16:37 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,7 +19,7 @@ function addTwoNumbers(number1,number2){
 }
 const result = addTwoNumbers(3,5)
 
-console.log(result);
+// console.log(result);
 
 function loginUserMessege(username){
     if(username === undefined) // if(!username) // works same
@@ -30,4 +30,34 @@ function loginUserMessege(username){
     return `${username} just logged in`
 }
 // console.log(loginUserMessege("Sohan"));
-console.log(loginUserMessege());
+// console.log(loginUserMessege());
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1 // o/p -> [500,2000] cz val1 and val2 got 200 and 400 || at last ... got remaining in num1
+}
+
+console.log(calculateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "Sohan",
+    price: "199"
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username:"Sohan",
+    price:"399"
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray)); // 400
+console.log(returnSecondValue([100,856,542,652])); // 856N
